d95a632b93bb195eb9c457176d9471df3a0e8d4d
marioinglese <m.inglese6@studenti.unisa.it>
2020-10-26 17:28:19 +0100

    home page

--- lib/home.dart
+++ lib/home.dart
@@ -5,21 +5,45 @@ import 'package:provider/provider.dart';
 
 import 'Screens/Quiz/quiz.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: KPrimaryColor,
       body: ListView(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(top: 35.0, left: 10.0),
+            padding: EdgeInsets.only(top: 5.0, left: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                    width: 125.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.logout),
+                          color: Colors.white,
+                          onPressed: () {
+                            context.read<AuthenticationService>().signOut();
+                          },
+                        ),
+                      ],
+                    )
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 10.0),
           ),
           SizedBox(height: 10.0),
           Padding(
@@ -48,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                 .size
                 .height-165,
             decoration: BoxDecoration(
-              color: Colors.white,
+              color: KPrimaryLightColor,
               borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
             ),
             child: ListView(
@@ -57,27 +81,136 @@ class _HomePageState extends State<HomePage> {
                 Padding(
                     padding: EdgeInsets.only(top: 45.0),
                 ),
-                RaisedButton(
-                  onPressed: () {
-                    context.read<AuthenticationService>().signOut();
-                  },
-                  child: Text("Sign out"),
+                Container(
+                  height: 90,
+                  width: size.width*0.8,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 1.0,
+                            color: Colors.black,
+                            spreadRadius: 0.5
+                        )
+                      ]
+                  ),
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text("Questionario conoscitivo",
+                                style: TextStyle(
+                                    fontFamily: 'Montserr',
+                                    fontSize: 20
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 9.0),
+                          Row(
+                            children: <Widget>[
+                              Text("Si prega di compliare il questionario",
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 15.0,
+                                    color: Colors.grey
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.arrow_forward_rounded,
+                            color: Colors.blue,
+                            size: 45.0,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-                RaisedButton(
-                  onPressed: () {Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context){
-                        return getjson();
-                      },
-                    ),
-                  );
-                  },
-                  child: Text("vai al quiz"),
-                )
+                SizedBox(height: 15.0),
+                Container(
+                  height: 90,
+                  width: size.width*0.8,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 1.0,
+                            color: Colors.black,
+                            spreadRadius: 0.5
+                        )
+                      ]
+                  ),
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text("Quiz",
+                                style: TextStyle(
+                                    fontFamily: 'Montserr',
+                                    fontSize: 20
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 9.0),
+                          Row(
+                            children: <Widget>[
+                              Text("Si prega di compliare il questionario",
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 15.0,
+                                    color: Colors.grey
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward_rounded,
+                              color: Colors.blue,
+                              size: 45.0,
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context){
+                                    return getjson();
+                                  },
+                                ),
+                              );
+                            },
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
