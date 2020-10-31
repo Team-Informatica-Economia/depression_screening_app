e1740f06eaa22702b85678cdae889386431773c1
marioinglese <m.inglese6@studenti.unisa.it>
2020-10-31 09:11:10 +0100

    aggiunta bottomNavigationBar

--- lib/home.dart
+++ lib/home.dart
@@ -24,10 +24,11 @@ class _HomePageState extends State<HomePage> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: KPrimaryColor,
+      extendBody: true,
       body: ListView(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(top: 5.0, left: 10.0),
+            padding: EdgeInsets.only(top: 0.0, left: 10.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
@@ -49,10 +50,6 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 10.0, left: 10.0),
-          ),
-          SizedBox(height: 10.0),
           Padding(
             padding: EdgeInsets.only(left: 40.0),
             child: Row(
@@ -85,13 +82,13 @@ class _HomePageState extends State<HomePage> {
             child: ListView(
               padding: EdgeInsets.only(left: 25.0, right: 20.0),
               children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(top: 45.0),
-                ),
+                /*Padding(
+                   padding: EdgeInsets.only(top: 45.0),
+               ),*/
                 SizedBox(height: 55.0),
                 InkWell(
                   child: Container(
-                    height: 135,
+                    //height: 135,
                     width: double.infinity,
                     child: Stack(
                       alignment: Alignment.bottomLeft,
@@ -143,12 +140,107 @@ class _HomePageState extends State<HomePage> {
                     );
                   },
                 ),
-
               ],
             ),
           ),
         ],
       ),
+      bottomNavigationBar: _getNavBar(context),
     );
   }
+  _getNavBar(context) {
+    return Container(
+      height: 120,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Positioned(
+              bottom: 0,
+              child:ClipPath(
+                clipper: NavBarClipper(),
+                child: Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.deepPurpleAccent,
+                            Colors.deepPurple,
+                          ]
+                      )
+                  ),
+                ),
+              )
+          ),
+          Positioned(
+            bottom: 45,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _buildNavItem(Icons.notifications),
+                SizedBox(width: 1,),
+                _buildNavItem(Icons.home),
+                SizedBox(width: 1,),
+                _buildNavItem(Icons.account_circle),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text("Notifiche",style: TextStyle(color: Colors.white.withOpacity(0.9),fontWeight: FontWeight.w500)),
+                SizedBox(width: 1,),
+                Text("Home",style: TextStyle(color: Colors.white.withOpacity(0.9),fontWeight: FontWeight.w500)),
+                SizedBox(width: 2,),
+                Text("Profilo",style: TextStyle(color: Colors.white.withOpacity(0.9),fontWeight: FontWeight.w500)),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+_buildNavItem(IconData icon){
+  return CircleAvatar(
+    radius: 30,
+    backgroundColor: Colors.amber,
+    child: CircleAvatar(
+      radius: 25,
+      backgroundColor: Colors.transparent,
+      child: Icon(
+        icon,
+        color: Colors.black,
+      ),
+    ),
+  );
+}
+class NavBarClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    var sw = size.width;
+    var sh = size.height;
+
+    path.cubicTo(sw/12, 0, sw/12, 2*sh/5, 2*sw/12, 2*sh/5);
+    path.cubicTo(3*sw/12, 2*sh/5, 3*sw/12, 0, 4*sw/12, 0);
+    path.cubicTo(5*sw/12, 0, 5*sw/12, 2*sh/5, 6*sw/12, 2*sh/5);
+    path.cubicTo(7*sw/12, 2*sh/5, 7*sw/12, 0, 8*sw/12, 0);
+    path.cubicTo(9*sw/12, 0, 9*sw/12, 2*sh/5, 10*sw/12, 2*sh/5);
+    path.cubicTo(11*sw/12, 2*sh/5, 11*sw/12, 0, sw, 0);
+    path.lineTo(sw, sh);
+    path.lineTo(0, sh);
+    path.close();
+    return path;
+
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
